3a99a56434f827f6e27ca36ecab886f65b4bb942
James McDermid <hello@jamesmcdermid.ca>
2015-08-13 13:49:44 -0400

    google font tags added

--- header.php
+++ header.php
@@ -3,10 +3,11 @@
 <head>
 	<?php // Load Meta ?>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
+  <!-- google fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,700,300,100,400italic' rel='stylesheet' type='text/css'>
   <?php // Load our CSS ?>
   <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
--- page-home.php
+++ page-home.php
@@ -4,20 +4,22 @@
 	Template Name: Home Page
 */
 
+//Custom Fields
+$hero_title = get_post_meta(6, 'hero_title', true);
+$hero_tagline = get_post_meta(6, 'hero_tagline', true);
+
+//get the header
 get_header();  ?>
 <main class="home-wrapper">
-	<div class="main">
-	  <div class="container">
 
-	    <?php // Start the loop ?>
-	    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<!-- =============================================
+HERO  
+==================================================-->
+	<section class="hero">
+		<h1><?php echo $hero_title; ?></h1>
+		<h2><?php echo $hero_tagline; ?></h2>
 
-	      <h2><?php the_title(); ?></h2>
-	      <?php the_content(); ?>
-
-	    <?php endwhile; // end the loop?>
-	  </div> <!-- /.container -->
-	</div> <!-- /.main -->
+	</section>
 </main><!--home-wrapper-->
 
 
