8f4e857ecb063acb1391fcda28f07b94a4a398f0
James McDermid <hello@jamesmcdermid.ca>
2015-08-17 13:18:25 -0400

    ready for deployment

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	<h1>
 		<a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
 		<!-- if user uploaded an image -->
-			<img src="assets/logo.png" alt="">
+			<img src="http://localhost:8888/bootcamp-projects/week-7-project/wp-content/uploads/2015/08/logo.png" alt="">
 
 		</a>
 	</h1>
